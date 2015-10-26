0530c20b9555fbee5b6d164e01b756bd121e655e
valeriechao <valeriechao@valeriechao-mbp.local>
2015-10-25 21:59:13 -0700

    Revert "Revert "Publishing""
    This reverts commit 83b58265faf6529306fa6e829f09812baae5a411.

---
+++ SignInRootViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignInRootViewController.swift
+//  Dropbox_v2
+//
+//  Created by Valerie Chao on 10/25/15.
+//  Copyright © 2015 Valerie Chao. All rights reserved.
+//
+
+import UIKit
+
+class SignInRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        performSegueWithIdentifier("signinSegue", sender: self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TermsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TermsViewController.swift
+//  Dropbox_v2
+//
+//  Created by Valerie Chao on 10/25/15.
+//  Copyright © 2015 Valerie Chao. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func doneButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
